55b2e44923bc24cb5be1d3848f5132cc2e6a77ab
zhantong1994 <zhantong1994@163.com>
2016-05-03 10:48:39 +0800

    复制自EnableSettings

---
+++ app/src/main/java/com/polarxiong/camerademo/MainActivity.java
@@ -0,0 +1,42 @@
+package com.polarxiong.camerademo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+/**
+ * Created by zhantong on 16/4/28.
+ */
+public class MainActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final CameraPreview mPreview = new CameraPreview(this);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+
+        SettingsFragment.passCamera(mPreview.getCameraInstance());
+        if (PreferenceManager.getDefaultSharedPreferences(this).getString(SettingsFragment.KEY_PREF_PREV_SIZE, null) == null) {
+            getFragmentManager().beginTransaction().replace(R.id.camera_preview, new SettingsFragment()).addToBackStack(null).commit();
+            getFragmentManager().executePendingTransactions();
+        }
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+        SettingsFragment.init(PreferenceManager.getDefaultSharedPreferences(this));
+        Button buttonSettings = (Button) findViewById(R.id.button_settings);
+        buttonSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.camera_preview, new SettingsFragment()).addToBackStack(null).commit();
+            }
+        });
+    }
+
+    public void onPause() {
+        finish();
+        super.onPause();
+    }
+}
